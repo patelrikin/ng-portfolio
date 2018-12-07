ec915a558d5155e99e865ae1cf5c9482cb653a89
Rikin Patel <rikin.patel@us.imshealth.com>
2018-12-06 21:23:07 -0500

    Updates module/component imports, additional imports were causing circular dependencies and causing build to fail. component specific routing module is imported in component module which is then imported in main app module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,40 +3,30 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AboutRoutingModule } from './about/about-routing.module';
-import { ContactRoutingModule } from './contact/contact-routing.module';
-import { PortfolioRoutingModule } from './portfolio/portfolio-routing.module';
-import { TechskillsRoutingModule } from './techskills/techskills-routing.module';
+import { AboutModule } from './about/about.module';
+import { ContactModule } from './contact/contact.module';
+import { PortfolioModule } from './portfolio/portfolio.module';
+import { TechskillsModule } from './techskills/techskills.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
-import { ContactComponent } from './contact/contact.component';
 import { DataService } from './data.service';
 import { FooterComponent } from './footer/footer.component';
-import { ClientComponent } from './portfolio/client/client.component';
-import { TechskillsComponent } from './techskills/techskills.component';
 import { WorkexpComponent } from './workexp/workexp.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent,
-    PortfolioComponent,
-    ContactComponent,
     FooterComponent,
-    ClientComponent,
-    TechskillsComponent,
     WorkexpComponent
   ],
   imports: [
     BrowserModule,
-    AboutRoutingModule,
-    ContactRoutingModule,
-    PortfolioRoutingModule,
-    TechskillsRoutingModule,
+    AboutModule,
+    ContactModule,
+    PortfolioModule,
+    TechskillsModule,
     AppRoutingModule,
     FormsModule
   ],
