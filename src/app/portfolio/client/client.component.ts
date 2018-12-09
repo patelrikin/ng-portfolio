33752e6a83564ed988567db795b1f949aed13d5e
Rikin Patel <rikin.patel@us.imshealth.com>
2018-12-08 23:47:25 -0500

    Adds little wip animation

--- src/app/portfolio/client/client.component.ts
+++ src/app/portfolio/client/client.component.ts
@@ -3,6 +3,13 @@ import { ActivatedRoute } from '@angular/router';
 
 import { DataService } from '../../data.service';
 
+export interface IClient {
+  name: string;
+  description: string;
+  clientURL: string;
+  image: string;
+}
+
 @Component({
   selector: 'app-client',
   templateUrl: './client.component.html',
@@ -10,14 +17,15 @@ import { DataService } from '../../data.service';
 })
 export class ClientComponent implements OnInit {
 
-  client: any;
+  client: IClient;
 
   constructor(private ds: DataService,
               private route: ActivatedRoute) { }
 
   ngOnInit() {
     const id = +this.route.snapshot.paramMap.get('id');
-    this.ds.getClient(id).subscribe(client => this.client = client);
+    this.ds.getClient(id).subscribe(client => {
+      this.client = client;
+    });
   }
-
 }
